3b6e3714a8cb6e8d56c1fa13dc0dc671bff9c512
yraffic02 <yraffic02@gmail.com>
2023-09-22 15:51:34 -0300

    feat: createdd component forms orders

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,23 @@
 export const FormsOrder = () => {
     return (
-        <div className="flex-col m-2 mt-4 h-full ">
+        <div className="flex-col m-2 mt-2">
+            <h3>tem alguma observação?</h3>
+            <input
+                className=" border-blue-500 border rounded-lg mt-2 p-1"
+                placeholder="Ex: hamburguer sem salada"
+            />
             <h3>Deseja enviar seu pedido?</h3>
             <button className="bg-blue-500 p-2 rounded-2xl text-white font-medium my-2">
                 Enviar
             </button>
-            <h3>tem alguma observação?</h3>
-            <textarea className=" border-blue-500 border rounded-lg" />
+            <div className="flex w-full justify-center">
+                <button className="bg-blue-500 p-2 rounded-2xl text-white font-medium my-2">
+                    Garçom quero encerrar
+                </button>
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
